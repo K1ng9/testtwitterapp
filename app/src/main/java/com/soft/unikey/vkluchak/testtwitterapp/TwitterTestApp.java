9551ae80f7b74eab3d85744da5ca99c0b5a0c458
K1ng9 <vkluchak@gmail.com>
2017-10-27 15:17:56 +0300

    update gradle

--- app/src/main/java/com/soft/unikey/vkluchak/testtwitterapp/TwitterTestApp.java
+++ app/src/main/java/com/soft/unikey/vkluchak/testtwitterapp/TwitterTestApp.java
@@ -2,19 +2,16 @@ package com.soft.unikey.vkluchak.testtwitterapp;
 
 import android.app.Application;
 import android.content.Context;
-import android.content.IntentFilter;
-import android.net.ConnectivityManager;
 
 import com.crashlytics.android.Crashlytics;
 import com.facebook.stetho.Stetho;
-import com.soft.unikey.vkluchak.testtwitterapp.app.receivers.NetworkReceiver;
 import com.soft.unikey.vkluchak.testtwitterapp.injection.component.ApplicationComponent;
 import com.soft.unikey.vkluchak.testtwitterapp.injection.component.DaggerApplicationComponent;
 import com.soft.unikey.vkluchak.testtwitterapp.injection.module.ApplicationModule;
-import io.fabric.sdk.android.Fabric;
-
 import com.twitter.sdk.android.Twitter;
 import com.twitter.sdk.android.core.TwitterAuthConfig;
+
+import io.fabric.sdk.android.Fabric;
 import timber.log.Timber;
 
 /**
@@ -27,12 +24,14 @@ public class TwitterTestApp extends Application {
     private ApplicationComponent mApplicationComponent;
 
 
-    private static final String TWITTER_KEY = "CncvECzq4uBkrSzSmDAh8DphB";
-    private static final String TWITTER_SECRET = "LXkpMgyyiBIFITlz169WwDUXfvWwYwxJ1DjJqIxn0tQC5Tzz1x";
+    private static String TWITTER_KEY;
+    private static String TWITTER_SECRET;
 
     public void onCreate() {
         super.onCreate();
-
+        // init keys
+        TWITTER_KEY = getString(R.string.twitter_key);
+        TWITTER_SECRET = getString(R.string.twitter_secret);
 
         TwitterAuthConfig authConfig = new TwitterAuthConfig(TWITTER_KEY, TWITTER_SECRET);
         Fabric.with(getApplicationContext(), new Twitter(authConfig), new Crashlytics());
---
+++ app/src/main/java/com/soft/unikey/vkluchak/testtwitterapp/data/repository/TweetRepository.java
@@ -0,0 +1,8 @@
+package com.soft.unikey.vkluchak.testtwitterapp.data.repository;
+
+/**
+ * Created by new on 26.10.17.
+ */
+
+public class TweetRepository {
+}
